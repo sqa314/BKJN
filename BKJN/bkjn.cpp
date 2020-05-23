b2e0f482fc9435bfe3864ea41ff11f186a70bfac
이창훈 <sqa314@naver.com>
2020-05-24 03:27:52 +0900

    1365

--- BKJN/bkjn.cpp
+++ BKJN/bkjn.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello, World!";
+	int i, n;
+	int temp;
+	vector<int> line;
+	cin >> n;
+	for (i = 0; i < n; ++i)
+	{
+		cin >> temp;
+		if (line.empty() || line.back() < temp)
+			line.push_back(temp);
+		else
+			*lower_bound(line.begin(), line.end(), temp) = temp;
+	}
+	cout << n - line.size();
 	return 0;
 }
