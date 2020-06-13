2ca85778f71b090f42990d4042bafad4b7ee468f
이창훈 <sqa314@naver.com>
2020-06-13 14:00:49 +0900

    dp

--- BKJN/bkjn.cpp
+++ BKJN/bkjn.cpp
@@ -1,9 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
 using namespace std;
+
+int A[10000];
+int B[10000];
+vector<int> C(10000);
 
 int main()
 {
-	cout << "Hello, World!";
+	int i, j;
+	int sum = 0;
+	int n, m;
+	int up;
+	scanf("%d %d", &n, &m);
+	for (i = 0; i < n; ++i)
+		scanf("%d", &A[i]);
+	for (i = 0; i < n; ++i)
+	{
+		scanf("%d", &B[i]);
+		sum += B[i];
+	}
+	for (i = 0; i < n; ++i)
+		for (j = sum; j - B[i] >= 0; --j)
+			C[j] = max(C[j], C[j - B[i]] + A[i]);
+	for (i = 0; i <= sum; ++i)
+		if (m <= C[i])
+		{
+			printf("%d", i);
+			break;
+		}
 	return 0;
 }
